c6ffc9c0096438bde6d092106d692de7ca4b24c0
mindsgn <seni.tembe@protonmail.com>
2021-09-11 19:49:45 +0200

    storage contract

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,53 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
+  // Name;
+  string public name = "decentralized File Storage";
   // Number of files
   // Mapping fileId=>Struct 
 
-  // Struct
+  uint public fileCount = 0;
 
+  mapping ( uint=> File) public files;
+
+  // Struct
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileName;
+    string fileType;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
   // Event
+  event FileUploaded(
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileName,
+    string fileType,
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+  );
 
   constructor() public {
   }
 
-  // Upload File function
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {  
+    require(bytes(_fileHash).length > 0);
+    require(bytes(_fileType).length > 0);
+    require(bytes(_fileDescription).length > 0);
+    require(bytes(_fileName).length > 0);
+    require(msg.sender!=address(0));
+    require(_fileSize>0);
 
-    // Make sure the file hash exists
+    fileCount += 1;
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileName, _fileType, _fileDescription, now, msg.sender);
 
-    // Make sure file type exists
-
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-
-    // Increment file id
-
-    // Add File to the contract
-
-    // Trigger an event
-
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileName, _fileType, _fileDescription, now, msg.sender);
+  }
 }
